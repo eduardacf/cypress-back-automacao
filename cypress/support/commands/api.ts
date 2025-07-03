79e7e044d650555fc69d10174721a0b91c4a5e83
Eduarda Ferreira <eduarda_26_duda@hotmail.com>
2025-07-03 10:22:27 -0300

    Add custom Cypress commands for CRUD operations on "Livro" resource

---
+++ cypress/support/commands/api.ts
@@ -0,0 +1,49 @@
+import {rotas} from "../../../utils/rotas";
+import { faker } from '@faker-js/faker';
+
+Cypress.Commands.add(
+    'validarContratoLivro',
+    (response: Cypress.Response<Livro>) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.all.keys(
+            'id',
+            'title',
+            'description',
+            'pageCount',
+            'excerpt',
+            'publishDate'
+        );
+    }
+);
+
+Cypress.Commands.add('criarLivro', (livro: Livro) => {
+    return cy.request('POST', rotas.livros.base, livro);
+});
+
+Cypress.Commands.add('deletarLivro', (id) => {
+   return cy.request('DELETE', rotas.livros.porId(id));
+});
+
+Cypress.Commands.add('editarLivro', (id,livro) => {
+return cy.request('PUT', rotas.livros.porId(id), livro);
+});
+
+Cypress.Commands.add('buscarLivro', (id: number, failOnError: boolean = true) => {
+    return cy.request({
+        method: 'GET',
+        url: rotas.livros.porId(id),
+        failOnStatusCode: failOnError
+    });
+});
+
+Cypress.Commands.add('gerarLivroFake', (): Cypress.Chainable<Livro> => {
+    const livro: Livro = {
+        id: faker.number.int({ min: 10000, max: 99999 }),
+        title: faker.lorem.words(3),
+        description: faker.lorem.sentence(),
+        pageCount: faker.number.int({ min: 50, max: 500 }),
+        excerpt: faker.lorem.paragraph(),
+        publishDate: new Date().toISOString()
+    };
+    return cy.wrap(livro);
+});
